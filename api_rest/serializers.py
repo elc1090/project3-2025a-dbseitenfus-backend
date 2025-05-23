637da30461d6020076897cb629b358484e3e1dea
Daniel Seitenfus <dbseitenfus@inf.ufsm.br>
2025-05-23 12:52:35 -0300

    feat: create user and document crud

--- api_rest/serializers.py
+++ api_rest/serializers.py
@@ -5,3 +5,8 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = '__all__'
+
+class DocumentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Document
+        fields = '__all__'
--- api_rest/views.py
+++ api_rest/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+
+from .models import User, Document
+from .serializers import UserSerializer, DocumentSerializer
+from rest_framework import viewsets
+
+
+class UserListViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+class DocumentViewSet(viewsets.ModelViewSet):
+    queryset = Document.objects.all()
+    serializer_class = UserSerializer
 
 # Create your views here.
+def databaseEmDjango():
+    data = User.objects.get(pk=1)
+    data = User.objects.filter(username='daniel')
