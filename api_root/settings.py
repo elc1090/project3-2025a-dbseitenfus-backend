e51851f2f3a2ee7dcdd95fd8c677e00558065f0f
Daniel Seitenfus <dbseitenfus@inf.ufsm.br>
2025-05-27 05:10:14 -0300

    fix: CORS issues in prodution

--- api_root/settings.py
+++ api_root/settings.py
@@ -135,8 +135,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # CORS settings
 CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOW_CREDENTIALS = True
-CORS_ALLOW_ALL_ORIGINS = False
+CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOWED_ORIGINS = [
+    "https://project3-2025a-dbseitenfus-frontend.vercel.app"
     "http://localhost:3000",
 ]
 
